c663ac7248f65e2cf6beaa21f3d2747368f910db
Oleksandr Chumakov <Chumakvs2001@gmail.com>
2025-04-04 19:21:15 +0300

    make build

---
+++ index.0eb6a593.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target;if("IMG"===n.tagName){var a=n.parentElement;a&&"A"===a.tagName&&(t.preventDefault(),e.src=a.href)}})});
+//# sourceMappingURL=index.0eb6a593.js.map
